7daf6f3578f5d75882db89e8093aba45e235cc5c
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-06 23:44:43 +0300

    Add env, config

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$env = parse_ini_file(realpath(__DIR__ . '/../../.env'));
+
+define('URL', '/');
+define('UPLOAD_MAX_SIZE', 1000000); 
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif', 'image/webp']);
+define('UPLOAD_DIR', 'uploads');
+
+define('SECRET_KEY', $env['SECRET_KEY']);
+define('DB_HOST', $env['DB_HOST']);
+define('DB_NAME', $env['DB_NAME']);
+define('DB_USER', $env['DB_USER']);
+define('DB_PASSWORD', $env['DB_PASSWORD']);
