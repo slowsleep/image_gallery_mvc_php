4dc6fd555dcb66c119db43fac4338cee94e4ee5e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-06 23:57:24 +0300

    Edit bootstrap: add require_once user model

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,5 +5,6 @@ require_once 'app/core/model.php';
 require_once 'app/core/controller.php';
 require_once 'app/core/view.php';
 require_once 'app/core/route.php';
+require_once 'app/models/user_model.php';
 
 Route::start();
