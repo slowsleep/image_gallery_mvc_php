b576e27d07e9b97ddaca09b4a307b389702becbd
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-06 23:47:57 +0300

    Add bootstrap file

---
+++ app/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'app/config/config.php';
+require_once 'app/core/model.php';
+require_once 'app/core/controller.php';
+require_once 'app/core/view.php';
+require_once 'app/core/route.php';
+
+Route::start();
