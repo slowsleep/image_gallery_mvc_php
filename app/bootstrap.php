405841baeec51e7c0419530a05904386eba0f33f
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-07 17:03:40 +0300

    Edit bootstrap php file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,8 @@ require_once 'app/core/model.php';
 require_once 'app/core/controller.php';
 require_once 'app/core/view.php';
 require_once 'app/core/route.php';
+require_once 'app/core/db.php';
 require_once 'app/models/user_model.php';
+require_once 'app/models/image_model.php';
 
 Route::start();
