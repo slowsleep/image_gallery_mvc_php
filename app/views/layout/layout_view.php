f11b222c030ce464c81a77797550f8e21190816b
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-07 20:48:11 +0300

    Edit layout view

--- app/views/layout/layout_view.php
+++ app/views/layout/layout_view.php
@@ -1,3 +1,6 @@
+<?php
+    $user = User_Model::check();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="../../../favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title><?php echo $title ?></title>
 </head>
 
@@ -19,8 +23,12 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <?php if ($user): ?>
+                        <p class="m-0">Привет, <?php echo $user['name'] ?>!</p>
+                    <?php endif; ?>
+
                     <ul class="navbar-nav">
-                        <?php if (!isset($_COOKIE['id']) && !isset($_COOKIE['hash'])): ?>
+                        <?php if (!$user): ?>
                             <li class="nav-item">
                                 <a class="nav-link" aria-current="page" href="/?page=registration">Зарегестрироваться</a>
                             </li>
