0c8727738cdbf1a26ed8631a6bd7775083e3a056
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-07 20:52:38 +0300

    Edit uploadImage_script.js: check file size, redirect to main on success

--- app/js/uploadImage_script.js
+++ app/js/uploadImage_script.js
@@ -1,8 +1,16 @@
 let uploadImageForm = document.querySelector("#uploadImageForm");
+let uploadResult = document.querySelector("#uploadResult");
+const UPLOAD_MAX_SIZE = 1000000;
 
 uploadImageForm.addEventListener("submit", function (event) {
     event.preventDefault();
     let formData = new FormData(uploadImageForm);
+
+    if (formData.get("image[]").size > UPLOAD_MAX_SIZE) {
+        uploadImageForm.querySelector("[name='image[]']").classList.add("is-invalid");
+        uploadResult.innerHTML = "Максимальный размер файла: " + UPLOAD_MAX_SIZE / 1000000 + "Мб.";
+        return;
+    }
 
     fetch("app/handlers/uploadImage_handler.php", {
         method: "POST",
@@ -10,19 +18,13 @@ uploadImageForm.addEventListener("submit", function (event) {
     }).then(function (response) {
         return response.json();
     }).then(function (result) {
-        let gallery = document.querySelector("#gallery");
-        let uploadResult = document.querySelector("#uploadResult");
         uploadImageForm.querySelectorAll(".is-invalid").forEach(function (element) {
             element.classList.remove("is-invalid");
         });
         uploadResult.innerHTML = "";
 
         if (result.status == "success") {
-            let filename = result.data;
-            const myImage = new Image(200, 200);
-            myImage.src = "/app/uploads/" + filename;
-            myImage.classList.add("img-thumbnail", "rounded");
-            gallery.appendChild(myImage);
+            window.location.href = "/";
             uploadResult.innerHTML = "Изображение успешно загружено";
         } else if (result.status == "error") {
             for (let key of result.data) {
