9ea7ed64be58ad6b946d44f49401abc64d0dd0e7
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-07 20:47:41 +0300

    Add methods getImageById and deleteImage to image model

--- app/models/image_model.php
+++ app/models/image_model.php
@@ -33,4 +33,31 @@ class Image_Model extends Model
         $images = $stmt->fetchAll();
         return $images;
     }
+
+    public static function getImageById($id)
+    {
+        $query = "SELECT * FROM images WHERE id = :id";
+        $db = new DB();
+        $db = $db->connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute(['id' => $id]);
+        $image = $stmt->fetch();
+        if ($stmt) {
+            return $image;
+        }
+        return false;
+    }
+
+    public static function deleteImage($id)
+    {
+        $query = "DELETE FROM images WHERE id = :id";
+        $db = new DB();
+        $db = $db->connect();
+        $stmt = $db->prepare($query);
+        $stmt->execute(['id' => $id]);
+        if ($stmt) {
+            return true;
+        }
+        return false;
+    }
 }
